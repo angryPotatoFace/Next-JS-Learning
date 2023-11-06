9c6fd21bac6110effa6602176ec2b6f0bdec1e48
Bruno Daniel Bonino <bruno.Bonino@ibm.com>
2023-11-06 16:17:16 -0300

    First Commit

---
+++ Primer app/components/layouts/DarkLayout.tsx
@@ -0,0 +1,16 @@
+import { ScriptProps } from 'next/script';
+
+export const DarkLayout: React.FC<ScriptProps > = ({ children }) => {
+  return (
+    <div style={{
+        backgroundColor: 'rgba(0,0,0,0.3)',
+        borderRadius: '5px',
+        padding: '10px',
+    }}>
+        <h3>Dark-Layout</h3>
+        <div>
+            { children } 
+        </div>
+    </div>
+  );
+};
